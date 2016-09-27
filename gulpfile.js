a5c09ba5376e89221611a9c8a9d2907269d615d4
malithJKMT <malit.tilak@gmail.com>
2016-09-27 11:34:02 +0530

    Integrate gulp

---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+var minify = require('gulp-minify');
+var stripDebug = require('gulp-strip-debug');
+var rename = require('gulp-rename');
+
+gulp.task('default', function () {
+  gulp.src(['src/**/module.js', 'src/**/*.js'])
+      .pipe(concat('angular-turn.debug.js'))      
+        .pipe(gulp.dest('./dist'))  
+         .pipe(rename('angular-turn.js'))
+        .pipe(stripDebug())   
+		.pipe(minify({
+		        ext:{
+		            src:'.js',
+		            min:'.min.js'
+		        }
+		    }))
+		    .pipe(gulp.dest('./dist'))
+		})
+
+gulp.task('watch', ['default'], function () {
+  gulp.watch('src/**/*.js', ['default'])
+})
