6b21c0bdc9dcb050bde8f631273f4a8060105b43
Tharindu-Diluksha <tdiluksha@gmail.com>
2016-09-28 11:44:06 +0530

    remove example code in source file book directive

--- src/Directives/book.js
+++ src/Directives/book.js
@@ -7,8 +7,6 @@
     directive.template = "<h1>Made by a directive!</h1>";
     directive.compile = function (element, attributes) {
         var linkFunction = function ($scope, element, attributes) {
-        	var bookTitle = attributes.ngbTitle;
-            element.html("<h1>" + bookTitle + "</h1>");
         }
         return linkFunction;
     }
