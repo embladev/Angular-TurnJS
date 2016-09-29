3d247e3b2770a3057a46c6801b1a6628b6cc3989
unknown <TharinduS@ITEPCUD003.embla.lk>
2016-09-29 18:19:31 +0530

    Flipbook structure partially completed

--- dist/angular-turn.debug.js
+++ dist/angular-turn.debug.js
@@ -2,7 +2,7 @@ angular.module("angularTurn",[]);
 
 
 (function(){
-	var bookDir = function(){
+	var bookDir = function($filter){
 		return {
             restrict: 'E',
 			link: function(scope, element, attrs) {
@@ -14,18 +14,34 @@ angular.module("angularTurn",[]);
                     post: function (scope, $element, attrs) {
                         console.log($("book").contents());
                         console.log($(document.getElementsByTagName("page")));
-                        console.log($(document.getElementsByTagName("page"))[0].childNodes);
+                        //console.log($(document.getElementsByTagName("page"))[0].childNodes);
+                        var pageContents = [];
+                        angular.forEach($(document.getElementsByTagName("page")), function(value,key){
+                            //console.log($filter('filter')(value.childNodes,"div"));
+                            pageContents.push($filter('filter')(value.childNodes,"div")[0]);
+                        });
+
+                        $("book").remove();
+                        var bookdiv = angular.element("div");
+                        bookdiv.attr("id","flipbook");
+
+                        console.log(bookdiv);
+
+                        angular.forEach(pageContents,function(value,key){
+                            bookdiv.append(value)
+                           console.log(value);
+                        });
                         //console.log(document.getElementById("qwe").childNodes[1]);
-                        $element.replaceWith($('<div id="flipbook"><div style="background-color:aqua"> Page 1</div><div style="background-color:aqua"> Page 2</div><div style="background-color:aqua"> Page 3</div><div style="background-color:aqua"> Page 4</div></div>'))
+                        //$element.replaceWith($('<div id="flipbook"><div style="background-color:aqua"> Page 1</div><div style="background-color:aqua"> Page 2</div><div style="background-color:aqua"> Page 3</div><div style="background-color:aqua"> Page 4</div></div>'))
                         //$("#flipbook").appendChild($('<div style="background-color:aqua"> Page 1</div><div style="background-color:aqua"> Page 2</div><div style="background-color:aqua"> Page 3</div><div style="background-color:aqua"> Page 4</div>'))
                         //element.outerHTML += '<div id="flipbook"><div style="background-color:aqua"> Page 1</div><div style="background-color:aqua"> Page 2</div><div style="background-color:aqua"> Page 3</div><div style="background-color:aqua"> Page 4</div></div>';
                         console.log(document.getElementById("flipbook"));
 
-                        $("#flipbook").turn({
+                        /*$("#flipbook").turn({
                             width: attrs.ngbWidth,
                             height: attrs.ngbHeight,
                             autoCenter: attrs.ngbAutocenter
-                        });
+                        });*/
                     }
                 }
             }
--- dist/angular-turn.js
+++ dist/angular-turn.js
@@ -2,7 +2,7 @@ angular.module("angularTurn",[]);
 
 
 (function(){
-	var bookDir = function(){
+	var bookDir = function($filter){
 		return {
             restrict: 'E',
 			link: function(scope, element, attrs) {
@@ -14,18 +14,34 @@ angular.module("angularTurn",[]);
                     post: function (scope, $element, attrs) {
                         void 0;
                         void 0;
+                        //console.log($(document.getElementsByTagName("page"))[0].childNodes);
+                        var pageContents = [];
+                        angular.forEach($(document.getElementsByTagName("page")), function(value,key){
+                            //console.log($filter('filter')(value.childNodes,"div"));
+                            pageContents.push($filter('filter')(value.childNodes,"div")[0]);
+                        });
+
+                        $("book").remove();
+                        var bookdiv = angular.element("div");
+                        bookdiv.attr("id","flipbook");
+
                         void 0;
+
+                        angular.forEach(pageContents,function(value,key){
+                            bookdiv.append(value)
+                           void 0;
+                        });
                         //console.log(document.getElementById("qwe").childNodes[1]);
-                        $element.replaceWith($('<div id="flipbook"><div style="background-color:aqua"> Page 1</div><div style="background-color:aqua"> Page 2</div><div style="background-color:aqua"> Page 3</div><div style="background-color:aqua"> Page 4</div></div>'))
+                        //$element.replaceWith($('<div id="flipbook"><div style="background-color:aqua"> Page 1</div><div style="background-color:aqua"> Page 2</div><div style="background-color:aqua"> Page 3</div><div style="background-color:aqua"> Page 4</div></div>'))
                         //$("#flipbook").appendChild($('<div style="background-color:aqua"> Page 1</div><div style="background-color:aqua"> Page 2</div><div style="background-color:aqua"> Page 3</div><div style="background-color:aqua"> Page 4</div>'))
                         //element.outerHTML += '<div id="flipbook"><div style="background-color:aqua"> Page 1</div><div style="background-color:aqua"> Page 2</div><div style="background-color:aqua"> Page 3</div><div style="background-color:aqua"> Page 4</div></div>';
                         void 0;
 
-                        $("#flipbook").turn({
+                        /*$("#flipbook").turn({
                             width: attrs.ngbWidth,
                             height: attrs.ngbHeight,
                             autoCenter: attrs.ngbAutocenter
-                        });
+                        });*/
                     }
                 }
             }
