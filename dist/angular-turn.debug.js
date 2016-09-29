7dbbe0c74a7a6f4b84fb50da6f40bb3ac7f949a9
unknown <TharinduS@ITEPCUD003.embla.lk>
2016-09-29 18:58:48 +0530

    Working turn book

--- dist/angular-turn.debug.js
+++ dist/angular-turn.debug.js
@@ -21,27 +21,30 @@ angular.module("angularTurn",[]);
                             pageContents.push($filter('filter')(value.childNodes,"div")[0]);
                         });
 
-                        $("book").remove();
-                        var bookdiv = angular.element("div");
-                        bookdiv.attr("id","flipbook");
-
+                        //$("book").remove();
+                        //var bookdiv = angular.element("book");
+                        $("book").replaceWith($('<div id="flipbook"></div>'));
+                        //bookdiv.attr("id","flipbook");
+                        var bookdiv = $(document.getElementById("flipbook"));
+                        //console.log();
                         console.log(bookdiv);
 
                         angular.forEach(pageContents,function(value,key){
                             bookdiv.append(value)
                            console.log(value);
                         });
+                        console.log(bookdiv);
                         //console.log(document.getElementById("qwe").childNodes[1]);
                         //$element.replaceWith($('<div id="flipbook"><div style="background-color:aqua"> Page 1</div><div style="background-color:aqua"> Page 2</div><div style="background-color:aqua"> Page 3</div><div style="background-color:aqua"> Page 4</div></div>'))
                         //$("#flipbook").appendChild($('<div style="background-color:aqua"> Page 1</div><div style="background-color:aqua"> Page 2</div><div style="background-color:aqua"> Page 3</div><div style="background-color:aqua"> Page 4</div>'))
                         //element.outerHTML += '<div id="flipbook"><div style="background-color:aqua"> Page 1</div><div style="background-color:aqua"> Page 2</div><div style="background-color:aqua"> Page 3</div><div style="background-color:aqua"> Page 4</div></div>';
                         console.log(document.getElementById("flipbook"));
 
-                        /*$("#flipbook").turn({
+                        $("#flipbook").turn({
                             width: attrs.ngbWidth,
                             height: attrs.ngbHeight,
                             autoCenter: attrs.ngbAutocenter
-                        });*/
+                        });
                     }
                 }
             }
