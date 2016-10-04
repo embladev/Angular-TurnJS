f770a86138a3560a9f3d17d61c684d736699e79a
malithJKMT <malit.tilak@gmail.com>
2016-10-04 13:50:30 +0530

    Divide example in to separate modules
    add new cover page example

--- src/Directives/cover.js
+++ src/Directives/cover.js
@@ -2,19 +2,17 @@
 
     var isTemplateGiven = false;
 
-
-
-	var pageDir = function () {
-		var dir = {
+    var coverDir = function () {
+        var dir = {
             restrict: 'E',
+            link: function(scope, element, attrs) {
+            },
             scope: {},
-
             compile: function (scope, element, attrs) {
                 return {
                     pre: function (scope, element, attrs) {
                         if("ngbTemplate" in attrs){
                             dir.templateUrl = function (element, attrs) {
-
                                 return attrs.ngbTemplate;
                             }
                         }
@@ -24,13 +22,11 @@
                         if("ngbTemplate" in attrs){
                             isTemplateGiven = true;
                             dir.templateUrl = function (element, attrs) {
-
                                 return attrs.ngbTemplate;
                             }
                         } else {
                             isTemplateGiven = false;
                         }
-                        console.log(" ");
                     }
                 }
             }
@@ -38,13 +34,14 @@
 
         if(isTemplateGiven){
             dir.templateUrl = function (element, attrs) {
-                    return attrs.ngbTemplate;
+                return attrs.ngbTemplate;
             }
         }
 
-        console.log(dir);
         return dir;
 
-	}	
-	angular.module("angularTurn").directive('page', pageDir);
+    }
+
+    angular.module("angularTurn").directive('cover', coverDir);
+
 })();
