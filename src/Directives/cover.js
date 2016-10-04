777b84ae22aa8654547e85232490af2b7f806435
bsc2fast <bsc2fast@hotmail.com>
2016-10-04 11:23:46 +0530

    Cover directive implemented

---
+++ src/Directives/cover.js
@@ -0,0 +1,47 @@
+(function(){
+
+    var isTemplateGiven = false;
+
+	var coverDir = function () {
+		var dir = {
+            restrict: 'E',
+			link: function(scope, element, attrs) {
+			},
+            scope: {},
+            compile: function (scope, element, attrs) {
+                return {
+                    pre: function (scope, element, attrs) {
+                        if("ngbTemplate" in attrs){
+                            dir.templateUrl = function (element, attrs) {
+                                return attrs.ngbTemplate;
+                            }
+                        }
+                    },
+                    post: function (scope, element, attrs) {
+                        isTemplateGiven = false;
+                        if("ngbTemplate" in attrs){
+                            isTemplateGiven = true;
+                            dir.templateUrl = function (element, attrs) {
+                                return attrs.ngbTemplate;
+                            }
+                        } else {
+                            isTemplateGiven = false;
+                        }
+                    }
+                }
+            }
+        }
+
+        if(isTemplateGiven){
+            dir.templateUrl = function (element, attrs) {
+                    return attrs.ngbTemplate;
+            }
+        }
+
+        return dir;
+
+	}
+
+	angular.module("angularTurn").directive('cover', coverDir);
+
+})();
