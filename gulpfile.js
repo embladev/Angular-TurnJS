e4b6187c06f9a097e58a1bfda4eca8f739a24e27
Chandimal Wickramaratne <chandimal@embla.lk>
2016-10-27 09:15:37 +0530

    updated glup with local server

--- gulpfile.js
+++ gulpfile.js
@@ -22,3 +22,22 @@ gulp.task('default', function () {
 gulp.task('watch', ['default'], function () {
   gulp.watch('src/**/*.js', ['default'])
 })
+
+var serverConfig = {
+    root: ".",
+    port: 9001
+};
+/*
+ *
+ *         Start standalone server
+ *
+ */
+gulp.task('server', function () {
+    connect.server(serverConfig);
+    //open('http://localhost:' + serverConfig.port);
+});
+
+// ---------------------------------------------- BUILD ----------------------------------------------
+gulp.task('test-server', ['watch', 'server'], function () {
+    console.log("Starting test server");
+});
